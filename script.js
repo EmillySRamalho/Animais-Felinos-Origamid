10d56f1d490c83ad2463fbd8ece5756abe807cc2
EmillySRamalho <emilly_bsfpb@hotmail.com>
2026-01-19 16:10:01 -0300

    Inner HTML ou Outer

--- script.js
+++ script.js
@@ -1,46 +1,10 @@
-const img = document.querySelector('img')
-
-function callback(){
-  console.log('Clicou na imagem')
-}
-
-img.addEventListener('click', callback)
-
-function callback2(event){
-  console.log(event)
-} 
-
-img.addEventListener('click', callback2)
-
-const animais = document.querySelector('.animais-lista')
-function callback3(event){
-  console.log(event.target)
-  console.log(event.currentTarget)
-  console.log(event.type)
-  console.log(this)
-}
-
-animais.addEventListener('click', callback3)
-
 const h1 = document.querySelector('h1')
+const animaisLista = document.querySelector('.animais-lista')
 
-function handleEvent(event){
-  console.log(event.type, event)
-} 
+console.log(h1.innerText);
+console.log(h1.outerHTML);
 
-h1.addEventListener('click', handleEvent)
-h1.addEventListener('mouseenter', handleEvent)
-h1.addEventListener('mousemove', handleEvent)
-h1.addEventListener('mouseout', handleEvent)
+h1.outerHTML = '<p>Felinos</p>'
 
-window.addEventListener('scroll', handleEvent)
-window.addEventListener('resize', handleEvent)
+console.log(animaisLista.innerText);
 
-function handleKeyBoard(event){
-  if(event.key === 'a'){
-    document.body.classList.toggle('azul')
-  } else if(event.key === 'v'){
-    document.body.classList.toggle('pink')
-  } 
-}
-  window.addEventListener('keydown', handleKeyBoard)
---
+++ seletores4.js
@@ -0,0 +1,46 @@
+const img = document.querySelector('img')
+
+function callback(){
+  console.log('Clicou na imagem')
+}
+
+img.addEventListener('click', callback)
+
+function callback2(event){
+  console.log(event)
+} 
+
+img.addEventListener('click', callback2)
+
+const animais = document.querySelector('.animais-lista')
+function callback3(event){
+  console.log(event.target)
+  console.log(event.currentTarget)
+  console.log(event.type)
+  console.log(this)
+}
+
+animais.addEventListener('click', callback3)
+
+const h1 = document.querySelector('h1')
+
+function handleEvent(event){
+  console.log(event.type, event)
+} 
+
+h1.addEventListener('click', handleEvent)
+h1.addEventListener('mouseenter', handleEvent)
+h1.addEventListener('mousemove', handleEvent)
+h1.addEventListener('mouseout', handleEvent)
+
+window.addEventListener('scroll', handleEvent)
+window.addEventListener('resize', handleEvent)
+
+function handleKeyBoard(event){
+  if(event.key === 'a'){
+    document.body.classList.toggle('azul')
+  } else if(event.key === 'v'){
+    document.body.classList.toggle('pink')
+  } 
+}
+  window.addEventListener('keydown', handleKeyBoard)
