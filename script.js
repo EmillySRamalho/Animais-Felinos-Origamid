73290b3b8fa856f6938ff5ce47367a9c9b41089f
EmillySRamalho <emilly_bsfpb@hotmail.com>
2026-01-19 15:43:56 -0300

    Event Listener

--- script.js
+++ script.js
@@ -16,7 +16,23 @@ const animais = document.querySelector('.animais-lista')
 function callback3(event){
   console.log(event.target)
   console.log(event.currentTarget)
-  console
+  console.log(event.type)
+  console.log(this)
 }
 
 animais.addEventListener('click', callback3)
+
+const h1 = document.querySelector('h1')
+
+function handleEvent(event){
+  console.log(event.type, event)
+} 
+
+h1.addEventListener('click', handleEvent)
+h1.addEventListener('mouseenter', handleEvent)
+h1.addEventListener('mousemove', handleEvent)
+h1.addEventListener('mouseout', handleEvent)
+
+window.addEventListener('scroll', handleEvent)
+window.addEventListener('resize', handleEvent)
+
