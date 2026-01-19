13a0b954a16560ddeecb04d58092b945e2cff682
EmillySRamalho <emilly_bsfpb@hotmail.com>
2026-01-19 17:08:30 -0300

    Script de tabs

--- script.js
+++ script.js
@@ -1,38 +1,10 @@
-const h1 = document.querySelector('h1')
-const animaisLista = document.querySelector('.animais-lista');
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-console.log(h1.innerText);
-console.log(h1.outerHTML);
+function activeTab(index) {
+  tabContent.forEach((section) => {
+    section.classList.remove('ativo');
+  });
+  tabContent[index].classList.add('ativo');   
+}
 
-h1.outerHTML = '<p>Felinos</p>'
-console.log(animaisLista.innerText);
-
-const lista = document.querySelector('.animais-lista');
-console.log(lista.parentElement)
-console.log(lista.parentElement.parentElement)
-console.log(lista.nextElementSibling)
-console.log(lista.previousElementSibling)
-
-console.log(lista.children[lista.children.length -1])
-
-console.log(lista.childNodes)
-
-const contato = document.querySelector('.contato');
-const titulo = contato.querySelector('.titulo');
-const animais = document.querySelector('.animais');
-const mapa = document.querySelector('.mapa');
-contato.insertBefore(animais, titulo);
-
-const novoH1 = document.createElement('h1');
-novoH1.innerText = 'Felinos são legais';
-novoH1.classList.add('titulo');
-mapa.appendChild(novoH1);
-console.log(novoH1);
-
-const h1 = document.querySelector('h1');
-const faq = document.querySelector('.faq');
-
-const cloneH1 = h1.cloneNode(true);
-
-cloneH1.classList.add('pink');
-faq.appendChild(cloneH1);
