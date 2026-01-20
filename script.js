7eb0773419d366158af14d2d99f7ff5bb42786da
EmillySRamalho <emilly_bsfpb@hotmail.com>
2026-01-20 16:23:08 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -56,3 +56,25 @@ linksInternos.forEach((link) => {
 }); 
 }
 initScrollSuave();
+
+function initAnimacaoScroll() {
+const section = document.querySelectorAll('.js-scroll');
+const windowMetade = window.innerHeight * 0.6;
+
+function animaScroll() {
+  section.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const isSectionVisible = (sectionTop - windowMetade) < 0;
+    if (isSectionVisible) 
+      section.classList.add('ativo');
+     else 
+      section.classList.remove('ativo');
+    });
+}
+
+animaScroll();
+window.addEventListener('scroll', animaScroll);
+}
+
+
+initAnimacaoScroll();
