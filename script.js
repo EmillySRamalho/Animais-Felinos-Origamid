887114f09d57403355b879b079b9a88ee4d99421
EmillySRamalho <emilly_bsfpb@hotmail.com>
2026-01-26 15:50:31 -0300

    dataset

--- script.js
+++ script.js
@@ -1,80 +1,13 @@
-function initTabNav(){
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
-tabContent[0].classList.add('ativo');
+//const div = document.querySelector('div');
 
-function activeTab(index) {
-  tabContent.forEach((section) => {
-    section.classList.remove('ativo');
-  });
-  tabContent[index].classList.add('ativo');   
-}
+//console.log(div.dataset)
+//console.log(div.dataset.cor)
 
-tabMenu.forEach((itemMenu, index) => {
-  itemMenu.addEventListener('click', () => {
-    activeTab(index);
-  })
-});
-}
-initTabNav();
+const div = document.querySelector('[data-cor]');
+div.dataset.height = "1000";
 
-function initAccordion(){
+delete div.dataset.width;
 
-const accordionList = document.querySelectorAll('.js-accordion dt');
-accordionList[0].classList.add('ativo');
-accordionList[0].nextElementSibling.classList.add('ativo');
+div.dataset.totalHeight = "2000";
 
-function activeAccordion() {
-  this.classList.toggle('ativo');
-  this.nextElementSibling.classList.toggle('ativo');
-
-}
-
-accordionList.forEach((item) => {
-  item.addEventListener('click', activeAccordion);
-})
-}
-initAccordion();
-
-function initScrollSuave() {
-const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
-
-function scrollToSection(event) { 
-  event.preventDefault();
-  const href = event.currentTarget.getAttribute('href');
-  const section = document.querySelector(href);
-
-  section.scrollIntoView({
-    behavior: 'smooth',
-    block: 'start',
-  });
-
-}
-
-linksInternos.forEach((link) => {
-  link.addEventListener('click', scrollToSection);
-}); 
-}
-initScrollSuave();
-
-function initAnimacaoScroll() {
-const section = document.querySelectorAll('.js-scroll');
-const windowMetade = window.innerHeight * 0.6;
-
-function animaScroll() {
-  section.forEach((section) => {
-    const sectionTop = section.getBoundingClientRect().top;
-    const isSectionVisible = (sectionTop - windowMetade) < 0;
-    if (isSectionVisible) 
-      section.classList.add('ativo');
-     else 
-      section.classList.remove('ativo');
-    });
-}
-
-animaScroll();
-window.addEventListener('scroll', animaScroll);
-}
-
-
-initAnimacaoScroll();
+console.log(div.dataset)
